a3699e9ca44a7c2a67cf5b27f7a4fcdb2700f2a7
Max <max.alt@gmx.de>
2024-06-01 15:57:00 +0200

    created new project :homectrl

---
+++ homectrl/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hc-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homectrl';
+}
