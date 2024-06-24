d39fbb9af755f6c8189b9dd4a48c0388ba8b9f4c
Max <max.alt@gmx.de>
2024-06-24 22:25:03 +0200

    refresh data concept works

--- homectrl/src/app/statistics/statistics.component.ts
+++ homectrl/src/app/statistics/statistics.component.ts
@@ -16,6 +16,9 @@ export type ChartOptions = {
   chart: ApexChart;
   xaxis: ApexXAxis;
   title: ApexTitleSubtitle;
+  yaxis: ApexYAxis | ApexYAxis[];
+  fill: ApexFill;
+  tooltip: ApexTooltip;
 };
 
 @Component({
@@ -37,12 +40,7 @@ export class StatisticsComponent {
     this.chartOptions = {
       series: [
         {
-          name: "Temperature",
-          data: []
-        },
-        {
-          name: "Humidity",
-          data: []
+          data: [1,2,3]
         }
 
       ],
@@ -51,29 +49,108 @@ export class StatisticsComponent {
         type: "line"
       },
       title: {
-        text: "My First Angular Chart"
+
       },
       xaxis: {
+        categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+      },
+      yaxis: [
+        {
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#008FFB"
+          },
+          labels: {
+            style: {
 
-      }
+            }
+          },
+          title: {
+            text: "Income (thousand crores)",
+            style: {
+              color: "#008FFB"
+            }
+          },
+          tooltip: {
+            enabled: true
+          }
+        },
+        {
+          seriesName: "Income",
+          opposite: true,
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#00E396"
+          },
+          labels: {
+            style: {
+
+            }
+          },
+          title: {
+            text: "Operating Cashflow (thousand crores)",
+            style: {
+              color: "#00E396"
+            }
+          }
+        },
+        {
+          seriesName: "Revenue",
+          opposite: true,
+          axisTicks: {
+            show: true
+          },
+          axisBorder: {
+            show: true,
+            color: "#FEB019"
+          },
+          labels: {
+            style: {
+
+            }
+          },
+          title: {
+            text: "Revenue (thousand crores)",
+            style: {
+              color: "#FEB019"
+            }
+          }
+        }
+      ],
+
+
+
     };
 
-    this.service.getAlltemp().subscribe(temperature =>{this.temperature = temperature;}, );
-    this.service.getAllhum().subscribe(humidity =>{this.humidity = humidity});
 
   }
 
   fetchDataAndRenderChart() {
-
-    }
-
-
-
+    this.refreshData()
+  }
 
   refreshData() {
-    this.service.getAlltemp().subscribe(temperature =>{this.temperature = temperature;}, );
-    this.service.getAllhum().subscribe(humidity =>{this.humidity = humidity});
-    this.fetchDataAndRenderChart()
+    var data = this.randData();
+    this.chart.updateSeries([{
+        name: "pizza",
+        data: data
+      }])
+
+  }
+
+
+  randData(): number[] {
+    var arr = [];
+    for (var i = 0; i < 9; i++) {
+      arr.push(Math.floor(Math.random() * 200) + 1);
+    }
+    return arr;
   }
 }
 
